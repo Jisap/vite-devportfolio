410222b0b8c7723b6a584e946a608e29da1452e5
Jorge Matos <jisapjisap@gmail.com>
2026-05-12 17:55:53 +0200

    feat: create Blog section component with header and scroll tracking

--- src/sections/Blog.jsx
+++ src/sections/Blog.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { SectionHeader } from "../components/SectionHeader"
+import useInView from "../hooks/useInView"
+import { BLOGS } from "../data/blog"
+import { useState } from "react";
+
+
 
 const Blog = () => {
+  const { ref, inView } = useInView();
+  const [idx, setIdx] = useState(0);
+
+  const visible = 3;
+  const max = BLOGS.length = visible;
+
   return (
-    <div>Blog</div>
+    <>
+      <section ref={ref} id="blog" className="py-25 px-[5%] bg-bg2">
+        <div className="max-w-275 mx-auto">
+          <SectionHeader
+            label="Writing"
+            title="Blog Articles"
+            inView={inView}
+          />
+        </div>
+      </section>
+    </>
   )
 }
 
